b554a88a38fdacb1e6db09a7ead18258f8cd4a61
sherzod02 <kholmamatovsh09>
2022-05-08 19:18:02 +0500

    twiter analog with react js

---
+++ demo/src/components/AppHeader/AppHeader.js
@@ -0,0 +1,14 @@
+import "./AppHeader.css";
+
+const AppHeader = ({ liked, allPosts }) => {
+  return (
+    <div className="app-header d-flex">
+      <h1>Sherzod Kholmamatov</h1>
+      <h2>
+        {allPosts} posts, like {liked}
+      </h2>
+    </div>
+  );
+};
+
+export default AppHeader;
